4c2d14447f289273b6b46b93c8bac2729fb7d741
Kateryna <katyuha.mik@gmail.com>
2025-10-04 14:21:19 +0000

    task 4-6

--- SerialMV.cpp
+++ SerialMV.cpp
@@ -1,8 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctime>
+
 void DummyDataInitialization(double* pMatrix, double* pVector, int Size);
 void ProcessInitialization(double*& pMatrix, double*& pVector, double*& pResult, int& Size);
 void PrintMatrix(double* pMatrix, int RowCount, int ColCount);
 void PrintVector(double* pVector, int Size);
+void ProcessTermination(double* pMatrix,double* pVector,double* pResult);
+void ResultCalculation(double* pMatrix, double* pVector, double* pResult,
+int Size);
+void RandomDataInitialization (double* pMatrix,double* pVector,int Size);
 
 int main() {
     
@@ -10,14 +17,28 @@ int main() {
     double* pVector;
     double* pResult;
     int Size;
+    clock_t start, finish;
+    double duration;
 
     printf("Serial matrix-vector multiplication program\n");
     ProcessInitialization(pMatrix, pVector, pResult,Size);
 
     printf ("Initial Matrix: \n");
     PrintMatrix (pMatrix, Size, Size);
-    printf ("Initial Vector: \n");
-    PrintVector (pVector, Size);
+    /*printf ("Initial Vector: \n");
+    PrintVector (pVector, Size);*/
+
+    start = clock();
+    ResultCalculation(pMatrix, pVector, pResult, Size);
+    finish = clock();
+    duration = (finish - start) / double(CLOCKS_PER_SEC);
+    
+    printf("\nTime of execution: %f\n", duration);
+    /*printf ("\n Result Vector: \n");
+    PrintVector(pResult, Size);*/
+    printf("\n Time of execution: %f", duration);
+
+    ProcessTermination(pMatrix, pVector, pResult);
       
     printf("Press Enter to finish...");
     getchar();
@@ -38,7 +59,7 @@ void ProcessInitialization(double*& pMatrix, double*& pVector, double*& pResult,
     pVector = new double[Size];
     pResult = new double[Size];
     
-    DummyDataInitialization(pMatrix, pVector, Size);
+    RandomDataInitialization(pMatrix, pVector, Size);
 }
 
 void DummyDataInitialization(double* pMatrix, double* pVector, int Size) {
@@ -60,4 +81,30 @@ void PrintMatrix(double* pMatrix, int RowCount, int ColCount) {
 void PrintVector(double* pVector, int Size) {
     for (int i = 0; i < Size; i++)
         printf("%7.4f ", pVector[i]);
+}
+
+void ProcessTermination(double* pMatrix,double* pVector,double* pResult) {
+delete [] pMatrix;
+delete [] pVector;
+delete [] pResult;
+}
+
+void ResultCalculation(double* pMatrix, double* pVector, double* pResult,
+int Size) {
+int i, j; // Loop variables
+for (i=0; i<Size; i++) {
+pResult[i] = 0;
+for (j=0; j<Size; j++)
+pResult[i] += pMatrix[i*Size+j]*pVector[j];
+}
+}
+
+void RandomDataInitialization (double* pMatrix,double* pVector,int Size) {
+int i, j; // Loop variables
+srand(unsigned(clock()));
+for (i=0; i<Size; i++) {
+pVector[i] = rand()/double(1000);
+for (j=0; j<Size; j++)
+pMatrix[i*Size+j] = rand()/double(1000);
+}
 }
